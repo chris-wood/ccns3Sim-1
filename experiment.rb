2b6649bc49a1070c866427201a8dedf525af4a78
Christopher Wood <christopherwood07@gmail.com>
2017-03-18 09:36:28 -0700

    Experiment runner.

---
+++ experiment.rb
@@ -0,0 +1,13 @@
+times = [1000] #, 2000]
+num_contents = [10] #, 25, 50, 100, 1000]
+cache_fractions = [0.25] #, 0.5, 1.0, 2.0, 5.0]
+
+times.each{|time|
+    num_contents.each{|n|
+        cache_fractions.each{|cf|
+            data_file = "#{time}_#{n}_#{cf}"
+            run_result = `../../waf --run="ccnx-monitor --time=#{time} --numberContents=#{n} --cacheSize=#{(n * cf).to_i}" 2> #{data_file}.txt`
+            plot_result = `python dist_plotter.py #{data_file}.txt #{data_file}`
+        }
+    }
+}
